441cf09d4e98c2b3c0004be7fedab9bf403a8e24
batuservices <89794154+batuservices@users.noreply.github.com>
2022-02-12 01:53:48 +0300

    Update installsnapshot.sh

--- installsnapshot.sh
+++ installsnapshot.sh
@@ -2,7 +2,7 @@
 cd /
 cd home
 cd bscfullnode
-wget -q -O - "https://tf-dex-prod-public-snapshot.s3-accelerate.amazonaws.com/geth-20220202.tar.lz4?AWSAccessKeyId=AKIAYINE6SBQPUZDDRRO&Signature=GeCrrNGWwVwNVYTBuAhBiNaI01w%3D&Expires=1646431102" | tar -I lz4 -xvf -
+wget -q -O - "https://tf-dex-prod-public-snapshot.s3-accelerate.amazonaws.com/geth-20220210.tar.lz4?AWSAccessKeyId=AKIAYINE6SBQPUZDDRRO&Signature=3IMWqQRnAMXs3ll2IZY%2F0GQ6abg%3D&Expires=1647120499" | tar -I lz4 -xvf -
 mv server/data-seed/geth/chaindata bsc/mainnet/geth/chaindata
 mv server/data-seed/geth/triecache bsc/mainnet/geth/triecache
 rm -r server
