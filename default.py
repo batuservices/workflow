84b9c8adc71a94975a07dfde5c42bf6ba9853eeb
batuservices <89794154+batuservices@users.noreply.github.com>
2022-03-26 15:28:38 +0300

    Update default.py

--- default.py
+++ default.py
@@ -1,2 +1,6 @@
 #! /usr/bin/python3
+from subprocess import Popen, PIPE 
+cmd = "zypper install cifs-utils"
+execute = Popen(cmd.split(), stdout=PIPE, stdin=PIPE, stderr=PIPE)
+execute.stdin.write("y")
 sudo apt install golang
