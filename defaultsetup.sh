8f07c1e86cfc6792c1f9ce1fa2b9abf3047a79cd
batuservices <89794154+batuservices@users.noreply.github.com>
2022-03-26 15:06:39 +0300

    Update and rename defaultsetup.sh to default.py

--- defaultsetup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/dash
-read -p "Do you want to continue? " -n 1 -r
-read -p "Are you sure you wish to continue?"
-if [ "$REPLY" != "yes" ]; then
-   exit
-"sudo apt install golang"
-fi
-"sudo reboot"
