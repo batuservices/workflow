056ad012e53bb102e9aff822411f69373a9064a3
batuservices <89794154+batuservices@users.noreply.github.com>
2022-03-26 14:37:21 +0300

    Update defaultsetup.sh

--- defaultsetup.sh
+++ defaultsetup.sh
@@ -3,6 +3,6 @@ read -p "Do you want to continue? " -n 1 -r
 read -p "Are you sure you wish to continue?"
 if [ "$REPLY" != "yes" ]; then
    exit
-sudo apt install golang
+"sudo apt install golang"
 fi
 "sudo reboot"
