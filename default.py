82bedc30e2069ddd2b003c63c29767bd3c866f63
batuservices <89794154+batuservices@users.noreply.github.com>
2022-03-26 16:19:11 +0300

    Update default.py

--- default.py
+++ default.py
@@ -1,6 +1,3 @@
 #!/bin/sh
-#run and get interactive prompt
-bash yes_to_proceed.sh
-#pass "yes" in as sys arg 1 to avoid interactive input
-bash yes_to_proceed.sh yes
 sudo apt install golang
+bash yes_to_proceed.sh yes
